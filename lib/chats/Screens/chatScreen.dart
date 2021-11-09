6f7e1d4a50cef7b18b6f3c7b01869f9d7d456819
Harsh Shrivastava <alexshri940@gmail.com>
2021-11-09 22:39:01 +0530

    added  create group UI

--- lib/chats/Screens/chatScreen.dart
+++ lib/chats/Screens/chatScreen.dart
@@ -1,14 +1,9 @@
 import 'dart:convert';
 
-import 'package:app/chats/components/chatmodel.dart';
-import 'package:app/globalHelpers/constants.dart';
-import 'package:app/globalHelpers/filledOutline.dart';
 import 'package:app/globalHelpers/global-helper.dart';
-import 'package:app/globalHelpers/routes.dart';
 import 'package:app/models/userModel.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:velocity_x/src/extensions/context_ext.dart';
 
 import '../components/chat_card.dart';
 
@@ -18,10 +13,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   List<User> parseList(String responseBody) {
     final parsed =
-    jsonDecode(responseBody)["usersList"].cast<Map<String, dynamic>>();
+        jsonDecode(responseBody)["usersList"].cast<Map<String, dynamic>>();
     return parsed.map<User>((json) => User.fromJson(json)).toList();
   }
 
@@ -50,29 +44,29 @@ class _ChatScreenState extends State<ChatScreen> {
       return list;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: _fetchFriends(),
-      builder: (context,snapshot){
-        if(snapshot.connectionState == ConnectionState.done){
-          if(snapshot.hasData){
-            List<User> friends = snapshot.data;
-            return ListView.builder(
-                itemCount: friends.length,
-                itemBuilder: (context, index) => ChatCard(
-                  title: friends[index].name,
-                  subtitle: friends[index].address,
-                  type: 'user',
-                ));
+        future: _fetchFriends(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              List<User> friends = snapshot.data;
+              return ListView.builder(
+                  itemCount: friends.length,
+                  itemBuilder: (context, index) => ChatCard(
+                        title: friends[index].name,
+                        subtitle: friends[index].address,
+                        type: 'user',
+                      ));
+            }
           }
-        }
-        return Center(
-          child: CircularProgressIndicator(
-            color: Colors.deepOrange,
-          ),
-        );
-      }
-    );
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.deepOrange,
+            ),
+          );
+        });
   }
 }
